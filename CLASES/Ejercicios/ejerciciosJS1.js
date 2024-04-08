e269d6e8f14866c7f8b6c9a6c9be26fc7f517f45
defilippi82 <f.defilippi@gmail.com>
2024-04-08 20:44:34 -0300

    fede

--- CLASES/Ejercicios/ejerciciosJS1.js
+++ CLASES/Ejercicios/ejerciciosJS1.js
@@ -2,143 +2,82 @@
 // reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "null";
+const nuevaString = "Hola Fede";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 192;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = null * 4 === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 
-function devolverString(str) {
-  // "Return" la string provista: str
-  // Tu código:
-return(str)
-}
-console.log(devolverString("Cadena de texto")); 
+let devolverString = str => str;
+console.log(devolverString("Cadena de texto"));
 
 
-let suma = (x, y) => x+y
-console.log(suma(10,10));
+let suma = (x, y) => x + y;
+console.log(suma(10, 5));
 
 
-function resta(x, y) {
-  // Resta "x" de "y" y devuelve el valor
-  // Tu código:
+let resta = (x, y) => x - y;
+console.log(resta(10, 5));
 
-}
+let multiplica = (x, y) => x * y;
+console.log(multiplica(10, 5));
 
-function multiplica(x, y) {
-  // Multiplica "x" por "y" y devuelve el valor
-  // Tu código:
+let divide = (x, y) => x / y;
+console.log(divide(10, 5));
 
-}
+const sonIguales = (x, y) => (x === y ? true : false); console.log(sonIguales(10, 10));
+/* if (x===y){
+return true
+}else{
+  return false
+} */
 
-function divide(x, y) {
-  // Divide "x" entre "y" y devuelve el valor
-  // Tu código:
+const tienenMismaLongitud = (str1, str2) => str1.length === str2.length;
+console.log(tienenMismaLongitud("1234", "doce"));
 
-}
+const menosQueNoventa = (num) => num < 90; console.log(menosQueNoventa(89));
 
-function sonIguales(x, y) {
-  // Devuelve "true" si "x" e "y" son iguales
-  // De lo contrario, devuelve "false"
-  // Tu código:utilizar if y else
+const mayorQueCincuenta = (num) => num > 50; console.log(mayorQueCincuenta(55));
+
+const obtenerResto = (x, y) => x % y; console.log(obtenerResto(22, 7));
+
+const esPar = num => num % 2 === 0; console.log(esPar(5));
+
+const esImpar = num => num % 2 !== 0; console.log(esImpar(5));
+
+const elevarAlCuadrado = num => num * num; console.log(elevarAlCuadrado(4));
+
+const elevarAlCubo = num => num * num * num; console.log(elevarAlCubo(4));
 
 
-}
+const elevar = (num, exponent) => Math.pow(num, exponent); console.log(elevar(4, 3));
 
-function tienenMismaLongitud(str1, str2) {
-  // Devuelve "true" si las dos strings tienen la misma longitud
-  // De lo contrario, devuelve "false"
-  // Tu código:
+const redondearNumero = num => Math.round(num); console.log(redondearNumero(1.4));
 
-}
+const redondearHaciaArriba = num => Math.ceil(num); console.log(redondearHaciaArriba(6.9));
 
-function menosQueNoventa(num) {
-  // Devuelve "true" si el argumento de la función "num" es menor que noventa
-  // De lo contrario, devuelve "false"
-  // Tu código:
-
-}
-
-function mayorQueCincuenta(num) {
-  // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
-  // De lo contrario, devuelve "false"
-  // Tu código:
-
-}
-
-function obtenerResto(x, y) {
-  // Obten el resto de la división de "x" entre "y"
-  // Tu código:
-
-}
-
-function esPar(num) {
-  // Devuelve "true" si "num" es par
-  // De lo contrario, devuelve "false"
-  // Tu código:
-
-  
-}
-
-function esImpar(num) {
-  // Devuelve "true" si "num" es impar
-  // De lo contrario, devuelve "false"
-  // Tu código:
-
-}
-
-function elevarAlCuadrado(num) {
-  // Devuelve el valor de "num" elevado al cuadrado
-  // ojo: No es raiz cuadrada!
-  // Tu código:
-
-}
-
-function elevarAlCubo(num) {
-  // Devuelve el valor de "num" elevado al cubo
-  // Tu código:
-
-}
-
-function elevar(num, exponent) {
-  // Devuelve el valor de "num" elevado al exponente dado en "exponent"
-  // Tu código:
-
-}
-
-function redondearNumero(num) {
-  // Redondea "num" al entero más próximo y devuélvelo
-  // Tu código:
-
-}
-
-function redondearHaciaArriba(num) {
-  // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
-  // Tu código:
-
-}
-
-function numeroRandom() {
+const numeroRandom = () => Math.random(); console.log(numeroRandom());
+{
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
 
 }
 
-function esPositivo(numero) {
+const esPositivo = numero => numero === 0 ? false : numero > 0 ? "Es positivo" : "Es negativo"; console.log(esPositivo(0));
+{
   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
@@ -146,60 +85,83 @@ function esPositivo(numero) {
 
 }
 
-function agregarSimboloExclamacion(str) {
+const agregarSimboloExclamacion = (str) => str + "!"; console.log(agregarSimboloExclamacion("Hola"));
+const agregarSimboloExclamacion2 = (str) => '${str}!'; console.log(agregarSimboloExclamacion("Hola"));
+
+{
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
 
 }
 
-function combinarNombres(nombre, apellido) {
+const combinarNombres = (nombre, apellido) => nombre + " " + apellido; console.log(combinarNombres("Federico", "De Filippi"));
+const combinarNombres2 = (nombre, apellido) => '${nombre} ${apellido}'; console.log(combinarNombres("Federico", "De Filippi"));
+
+{
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Bruce Wayne" -> "Bruce Wayne"
   // Tu código:
 
 }
+const obtenerSaludo = (nombre) => "Hola " + nombre + "!"; console.log(obtenerSaludo("Federico"));
+const obtenerSaludo2 = (nombre) => 'Hola ${nombre} !'; console.log(obtenerSaludo("Federico"));
 
-function obtenerSaludo(nombre) {
+{
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
 
 }
 
-function obtenerAreaRectangulo(alto, ancho) {
+const obtenerAreaRectangulo = (alto, ancho) => alto * ancho; console.log(obtenerAreaRectangulo(10, 5));
+{
   // Retornar el area de un cuadrado teniendo su altura y ancho
   // Tu código:
 
 }
 
-function retornarPerimetro(lado){
+const retornarPerimetro = (lado) => lado * 4; console.log(retornarPerimetro(10));
+{
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
 
 }
 
-function areaDelTriangulo(base, altura){
+const areaDelTriangulo = (base, altura) => (base * altura) / 2; console.log(areaDelTriangulo(10, 5));
+{
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
-
 }
 
-function deEuroAdolar(euro){
+const deEuroAdolar = euro => euro * 1.20; console.log(deEuroAdolar(10));
+{
   //Supongamos que 1 euro equivale a 1.20 dólares. 
   //Escribe un programa que pida al usuario un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
 
 }
 
+//const esVocal = letra => ?letra.length!=1:ingresaste mas de una letra? letra.match(/[aeiou]/i)? Es vocal : Dato Incorrecto;console.log(esVocal("e"));
+const esVocal = (letra) => {
+  let vocales = ["a", "e", "i", "o", "u"];
+  if (letra.length === 1) {
+    for (i = 0; i < vocales.length; i++) {
+      if (letra.toLowerCase() == vocales[i]) {
+        return "es vocal";
+      }
+    }
+    return `dato incorrecto ingresaste un caracter incorrecto es la ${letra}`
 
-function esVocal(letra){
-  //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
-  //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
-  //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
-    //si ingresa una consonante muestre en pantalla dato incorrecto
-  //Escribe tu código aquí
+  } return `Dato Incorrecto ingresaste mas de un caracter ingresaste ${letra}`
+
+};
+console.log(esVocal("ph"));
+//Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
+//Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle
+//que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
+//si ingresa una consonante muestre en pantalla dato incorrecto
+//Escribe tu código aquí
 
 
-}
+
--- CLASES/Ejercicios/ejerciciosJS2.js
+++ CLASES/Ejercicios/ejerciciosJS2.js
@@ -1,22 +1,25 @@
 
 
-function obtenerMayor(x, y) {
+let obtenerMayor = (x, y) => Math.max(x, y); console.log(obtenerMayor(6, 2));
+{
   // "x" e "y" son números enteros .
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-/* Metodos de MAth */
+  /* Metodos de MAth */
 
 }
 
-function mayoriaDeEdad(edad) {
+let mayoriaDeEdad = edad => edad >= 18 ? "Allowed" : "Not allowed"; console.log(mayoriaDeEdad(20))
+{
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
 
 }
-  
-function conection(status) {
+
+let conection = status => status === 1 ? "Online" : status === 2 ? "Away" : "Offline"; console.log(conection(5))
+{
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
   //Cuando el estado es igual a 1, el usuario está "Online"
   //Cuando el estado es igual a 2, el usuario está "Away"
@@ -25,7 +28,8 @@ function conection(status) {
 
 }
 
-function saludo(idioma) {
+let saludo = idioma => idioma === "aleman" ? "Guten Tag!" : idioma === "mandarin" ? "Ni Hao!" : idioma === "ingles" ? "Hello!" : "Hola!"; console.log(saludo("ingles"))
+{
   // Devuelve un saludo en tres diferentes lenguajes:
   // Si "idioma" es "aleman", devuelve "Guten Tag!"
   // Si "idioma" es "mandarin", devuelve "Ni Hao!"
@@ -35,7 +39,8 @@ function saludo(idioma) {
 
 }
 
-function colors(color) {
+//let colors=color => color=== "blue"? "This is blue": color==="red"? "This is red": color==="green"? "This is green": color==="orange"? "This is orange": "Color not found"; console.log(colors("orange"))
+let colors = (color) => {
   //La función recibe un color. Devolver el string correspondiente:
   //En caso que el color recibido sea "blue", devuelve --> "This is blue"
   //En caso que el color recibido sea "red", devuelve --> "This is red"
@@ -43,25 +48,43 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuelve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-
+  switch (color) {
+    case "blue":
+      return "This is blue";
+    case "red":
+      return "This is red";
+    case "green":
+      return "This is green";
+    case "orange":
+      return "This is orange";
+    default:
+      return "Color not found";
+  }
 
 }
+console.log(colors("orange"));
 
-function esDiezOCinco(numero) {
+let esDiezOCinco = numero => numero === 10 || numero === 5; console.log(esDiezOCinco(10));
+{
+ // switch (numero) {
+
+  //}
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
 
 }
 
-function estaEnRango(numero) {
+let estaEnRango = numero => numero < 50 && numero > 20; console.log(estaEnRango(25))
+{
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
 
 }
 
-function esEntero(numero) {
+let esEntero = numero => numero === Math.floor(numero); console.log(esEntero(5));
+{
   // Devuelve "true" si "numero" es un entero (int/integer)
   // Ejemplo: 0.8 -> false
   // Ejemplo: 1 -> true
@@ -72,16 +95,39 @@ function esEntero(numero) {
 
 }
 
-function fizzBuzz(numero) {
+let fizzBuzz = numero => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  };
   // Si "numero" es divisible entre 3, devuelve "fizz"
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
 
 }
+console.log(fizzBuzz(5));
 
-
-function operadoresLogicos(num1, num2, num3) {
+let operadoresLogicos = (num1, num2, num3) => {
+  switch (true) {
+    case num1 === 0 || num2 === 0 || num3 === 0:
+      return "Error";
+    case num1 < 0 || num2 < 0 || num3 < 0:
+      return "Hay negativos";
+    case num1 > num2 && num1 > num3:
+      return "Número 1 es mayor y positivo";
+    case num3 > num1 && num3 > num2:
+      return num3 + 1;
+    default:
+      return false;
+  }
   //La función recibe tres números distintos. 
   //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
@@ -89,36 +135,74 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
 
-}
+};
+console.log(operadoresLogicos(5, 1, 9));
 
-function esPrimo(numero) {
+let esPrimo = (numero) =>
+ {
+  if (numero < 2) {
+    return false;
+}
+for (let i = 2; i <= Math.sqrt(numero); i++) {
+    if (numero % i === 0) {
+        return false;
+    }
+}
+return true;
+}
+console.log(esPrimo(5));
+
+// Ejemplos de uso:
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
 
-}
-
-function esVerdadero(valor){
+let esVerdadero= valor => valor === 5 ? "Soy verdadero" : "Soy falso"; console.log(esVerdadero(5));
+ {
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 
 }
 
-function tablaDelSeis(){
+let tablaDelSeis=()=> {
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
+  const tabla = [];
+  for (let i = 0; i <= 10; i++) {
+      tabla.push(6 * i);
+  }
+  return tabla;
+};
+console.log(tablaDelSeis());
 
-}
-
-function tieneTresDigitos(numero){
+let tieneTresDigitos = numero => {
   //Leer un número entero y determinar si tiene 3 dígitos.
   //Escribe tu código aquí
+  const numeroTresDigitos = numero.toString();
 
-}
+  // Verificamos si la longitud de la cadena es igual a 3
+  if (numeroTresDigitos.length === 3) {
+      return true;
+  } else {
+      return false;
+  }
+};console.log(tieneTresDigitos(456));
+let tieneTresDigitos2 = numero =>
+{
+  //Leer un número entero y determinar si tiene 3 dígitos.
+  //Escribe tu código aquí
+  
+  if (numero >99 && numero< 1000) {
+      return true;
+  } else {
+      return false;
+  }
+};console.log(tieneTresDigitos2(46));
+
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
